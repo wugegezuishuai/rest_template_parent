58d7d7fb5d4341291fe7bfab606be7ac559fc17d
Jianwu Wang <wangjianwu_it@foxmail.com>
2020-07-13 00:32:03 +0800

    restTemplate 高级用法

---
+++ rest-template-02/src/main/java/com/github/demo/model/Coffee.java
@@ -0,0 +1,26 @@
+package com.github.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.money.Money;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Jianwu Wang
+ * @dateTime 2020/7/13 0:14
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee implements Serializable {
+    private Long id;
+    private String name;
+    private Money price;
+    private Date createTime;
+    private Date updateTime;
+}
