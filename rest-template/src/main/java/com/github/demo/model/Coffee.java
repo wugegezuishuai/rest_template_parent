35fd10251cc2ad25409180af0edbbfb47de92de0
Jianwu Wang <wangjianwu_it@foxmail.com>
2020-07-13 00:09:50 +0800

    Initial commit

---
+++ rest-template/src/main/java/com/github/demo/model/Coffee.java
@@ -0,0 +1,27 @@
+package com.github.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author Jianwu Wang
+ * @dateTime 2020/7/12 23:55
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee implements Serializable {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Date createTime;
+    private Date updateTime;
+}
